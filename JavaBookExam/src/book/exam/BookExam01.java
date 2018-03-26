ccd7379a7df8319220199e4fd7996e8197ef6e66
YoungKim <kimyk0120@hotmail.com>
2018-03-26 18:17:28 +0900

    Signed-off-by: YoungKim <kimyk0120@hotmail.com>

--- JavaBookExam/src/book/exam/BookExam01.java
+++ JavaBookExam/src/book/exam/BookExam01.java
@@ -2,10 +2,15 @@ package book.exam;
 
 import java.util.*;
 
-public class BookExam01 {
+import book.exam.Card.Kind;
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+public class BookExam01 {
+	
+	
+
+//	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void main(String[] args) {
+
 		
 //		LocalDate date = LocalDate.now();
 //		System.out.println(date);
@@ -20,52 +25,82 @@ public class BookExam01 {
 //		
 		
 		
-		ArrayList list1 = new ArrayList(10);
-		list1.add(new Integer(5));
-		list1.add(new Integer(4));
-		list1.add(new Integer(2));
-		list1.add(new Integer(0));
-		list1.add(new Integer(1));
-		list1.add(new Integer(3));
-		System.out.println(list1);
+//		ArrayList list1 = new ArrayList(10);
+//		list1.add(new Integer(5));
+//		list1.add(new Integer(4));
+//		list1.add(new Integer(2));
+//		list1.add(new Integer(0));
+//		list1.add(new Integer(1));
+//		list1.add(new Integer(3));
+//		System.out.println(list1);
+//		
+//		ArrayList list2 = new ArrayList(list1.subList(1, 4));
+//		System.out.println(list2);
+//		
+//		Collections.sort(list1);
+//		Collections.sort(list2);
+//		System.out.println(list2);
+//		
+//		list2.add("B");
+//		list2.add("C");
+//		list2.add("A");
+//		System.out.println(list2);
+//		
+//		list2.add(3,"A");
+//		System.out.println(list2);
+//		System.out.println(list2);
+//		list2.set(3, "AA");
+//		
+//		System.out.println(list1.retainAll(list2));
+//		System.out.println(list1);
+//		
+//		list1.remove(1);
+//		System.out.println(list1);
+//		
+//		LinkedList l1 = new  LinkedList<>();
+//		l1.add("2");
+//		l1.add("1");
+//		System.out.println("\n"+l1);
+//		l1.addAll(list2);
+//		System.out.println(l1);
+//		
+//		
 		
-		ArrayList list2 = new ArrayList(list1.subList(1, 4));
-		System.out.println(list2);
 		
-		Collections.sort(list1);
-		Collections.sort(list2);
-		System.out.println(list2);
+//		List list = new ArrayList<>();
+//		for(int i=0;i<10;i++){
+//			list.add("test"+i);
+//		}
+////		System.out.println(list);
 		
-		list2.add("B");
-		list2.add("C");
-		list2.add("A");
-		System.out.println(list2);
+//		Iterator it = list.iterator();
+//		while(it.hasNext()){
+////			System.out.println(it.next());
+//		}
 		
-		list2.add(3,"A");
-		System.out.println(list2);
-		System.out.println(list2);
-		list2.set(3, "AA");
+//		HashMap map = new HashMap<>();
+//		map.put("id", 1234);
+//		map.put("asdf", 1111);
+//		map.put("asdf", 2222);
+//		System.out.println(map);
+//		System.out.println(map.get("id"));
 		
-		System.out.println(list1.retainAll(list2));
-		System.out.println(list1);
+		Kind d = Card.Kind.clover;
+		System.out.println(d);
 		
-		list1.remove(1);
-		System.out.println(list1);
+//		Kind s = Card.Kind;
+//		System.out.println(s);
 		
-		LinkedList l1 = new  LinkedList<>();
-		l1.add("2");
-		l1.add("1");
-		System.out.println("\n"+l1);
-		l1.addAll(list2);
-		System.out.println(l1);
+		
 		
 		
 		
 		
 	}//.main
-
+	
+	
+	
 }//.class
-
 
 
 
---
+++ JavaBookExam/src/book/exam/Card.java
@@ -0,0 +1,12 @@
+package book.exam;
+
+public class Card {
+	
+	enum Kind {clover,heart,diamond,spade}
+	enum Value {Two,Three,Four}
+	
+//	final Kind kind;
+//	final Value value;
+	
+	
+}
